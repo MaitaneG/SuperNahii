a3e152110ba2c32142621466572d633746fbd29f
Gallastegui Juan <gallastegui.maitane@uni.lan>
2021-12-13 12:41:06 +0100

    Aldaketak egiten

--- SuperNaaahiGame/app/src/main/java/com/example/supernaaahigame/objects/Reindeer.java
+++ SuperNaaahiGame/app/src/main/java/com/example/supernaaahigame/objects/Reindeer.java
@@ -1,25 +1,30 @@
 package com.example.supernaaahigame.objects;
 
-import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.example.supernaaahigame.R;
 
 public class Reindeer {
-    private ImageView argazkia;
+    private Bitmap argazkia;
     private int x;
     private int y;
     private int width;
     private int height;
 
-    public Reindeer(ImageView argazkia, int x, int y) {
-        this.argazkia = argazkia;
-        this.x = x;
-        this.y = y;
+    public Reindeer(Bitmap argazkia, int width, int height, int x, int y) {
+        this.argazkia= argazkia;
+        this.width=width;
+        this.height=height;
+        this.x=x;
+        this.y=y;
     }
 
-    public ImageView getArgazkia() {
+    public Bitmap getArgazkia() {
         return argazkia;
     }
 
-    public void setArgazkia(ImageView argazkia) {
+    public void setArgazkia(Bitmap argazkia) {
         this.argazkia = argazkia;
     }
 
@@ -28,7 +33,7 @@ public class Reindeer {
     }
 
     public void setX(int x) {
-        this.x = x;
+        this.x = x + 10;
     }
 
     public int getY() {
@@ -37,5 +42,21 @@ public class Reindeer {
 
     public void setY(int y) {
         this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 }

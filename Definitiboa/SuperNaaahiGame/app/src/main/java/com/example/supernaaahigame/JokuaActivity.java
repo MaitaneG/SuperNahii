a27626baebb9169ec042db540e9ea08349329063
Gallastegui Juan <gallastegui.maitane@uni.lan>
2021-12-16 12:11:27 +0100

    Berriro hasten

---
+++ Definitiboa/SuperNaaahiGame/app/src/main/java/com/example/supernaaahigame/JokuaActivity.java
@@ -0,0 +1,13 @@
+package com.example.supernaaahigame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JokuaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ Definitiboa/SuperNaaahiGame/app/src/main/java/com/example/supernaaahigame/JokuaView.java
@@ -0,0 +1,32 @@
+package com.example.supernaaahigame;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.SurfaceView;
+
+public class JokuaView extends SurfaceView implements Runnable {
+    private Thread haria;
+
+    public JokuaView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    private void resume(){
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+
+    private void sleep(){
+
+    }
+}

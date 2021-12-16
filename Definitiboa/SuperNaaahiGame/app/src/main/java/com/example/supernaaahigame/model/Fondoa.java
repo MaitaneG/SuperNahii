b22a76e8bfea39f159c399a403972cc3035f431a
Gallastegui Juan <gallastegui.maitane@uni.lan>
2021-12-16 14:22:08 +0100

    d

--- Definitiboa/SuperNaaahiGame/app/src/main/java/com/example/supernaaahigame/model/Fondoa.java
+++ Definitiboa/SuperNaaahiGame/app/src/main/java/com/example/supernaaahigame/model/Fondoa.java
@@ -7,11 +7,36 @@ import android.graphics.BitmapFactory;
 import com.example.supernaaahigame.R;
 
 public class Fondoa {
-    private int X, y, width, height;
+
+    private int x=0, y=0;
     private Bitmap fondoa;
 
     public Fondoa(int screenX, int screenY, Resources res){
-        fondoa= BitmapFactory.decodeResource(res, R.drawable.fondo_hasiera);
+        fondoa= BitmapFactory.decodeResource(res, R.drawable.fondo_bigarren_posizioa);
         fondoa= Bitmap.createScaledBitmap(fondoa,screenX,screenY,false);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Bitmap getFondoa() {
+        return fondoa;
+    }
+
+    public void setFondoa(Bitmap fondoa) {
+        this.fondoa = fondoa;
     }
 }
--- Definitiboa/SuperNaaahiGame/app/src/main/java/com/example/supernaaahigame/model/Rudolph.java
+++ Definitiboa/SuperNaaahiGame/app/src/main/java/com/example/supernaaahigame/model/Rudolph.java
@@ -9,19 +9,51 @@ import com.example.supernaaahigame.R;
 public class Rudolph {
 
     private int X, y, width, height;
-    private Bitmap reno1;
+    private Bitmap renoIbiltzen1, renoIbiltzen2, renoSaltoEgiten, renoMakurtzen;
 
     public Rudolph(int screenY, Resources res) {
-        reno1 = BitmapFactory.decodeResource(res, R.drawable.reno_andando);
+        renoIbiltzen1 = BitmapFactory.decodeResource(res, R.drawable.reno_andando);
+        renoSaltoEgiten = BitmapFactory.decodeResource(res, R.drawable.re);
+        renoIbiltzen1 = BitmapFactory.decodeResource(res, R.drawable.reno_andando);
 
-        width = reno1.getWidth();
-        height = reno1.getHeight();
+        width = renoIbiltzen1.getWidth();
+        height = renoIbiltzen1.getHeight();
 
         // Ez dakigu nola irtengo den
         width /= 4;
         height /= 4;
 
-
     }
 
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Bitmap getRenoIbiltzen1() {
+        return renoIbiltzen1;
+    }
+
+    public Bitmap getRenoIbiltzen2() {
+        return renoIbiltzen2;
+    }
+
+    public Bitmap getRenoSaltoEgiten() {
+        return renoSaltoEgiten;
+    }
+
+    public Bitmap getRenoMakurtzen() {
+        return renoMakurtzen;
+    }
 }

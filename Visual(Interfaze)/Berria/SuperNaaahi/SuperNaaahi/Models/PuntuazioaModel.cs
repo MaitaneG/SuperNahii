c3ccc85cf3adf263e511be1f1df0a09ce7b952e7
Oihana1 <oaranba.azkoitia@gmail.com>
2022-01-21 08:04:25 +0100

    C# puntuazio modela, puntuazioService

---
+++ Visual(Interfaze)/Berria/SuperNaaahi/SuperNaaahi/Models/PuntuazioaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperNaaahi.Models
+{
+    public class PuntuazioaModel
+    {
+        public int id { get; set; }
+        public string jokalaria { get; set; }
+        public int puntuazioa { get; set; }
+        public DateTime data { get; set; }
+
+    }
+}

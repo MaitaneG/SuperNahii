cb39e79347471766ce8509fc11ae0276b481a3e9
Gallastegui Juan <gallastegui.maitane@uni.lan>
2022-01-31 09:20:29 +0100

    REST APIa ondo

--- Datu Atzipena/REST_API_SuperNaaahi/src/main/java/eus/dam/uni/SpringFoxConfig.java
+++ Datu Atzipena/REST_API_SuperNaaahi/src/main/java/eus/dam/uni/SpringFoxConfig.java
@@ -11,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 
- * Swagger-a konfiguratzeko (momentuz ez da funtzionatzen)
+ * Swagger-a konfiguratzeko
  *
  */
 @Configuration
